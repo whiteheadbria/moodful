a2e05110b4953ba3816007b4e5ee98add411c5ba
Bria Whitehead <whitehead.bria@protonmail.com>
2023-02-01 06:55:20 -0600

    Moodful updated wit spring. 5am

---
+++ src/main/java/com/moodful/moodful/MoodfulApplication.java
@@ -0,0 +1,13 @@
+package com.moodful.moodful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodfulApplication.class, args);
+	}
+
+}
